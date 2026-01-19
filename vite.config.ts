6f76e2d7aa7db22a1f806d68e2b7c56986ed33ff
Harish Soni Swarnkar <hk220394@gmail.com>
2026-01-19 23:37:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      "Permissions-Policy": "unload=*",
+    },
+  },
+  preview: {
+    headers: {
+      "Permissions-Policy": "unload=*",
+    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
